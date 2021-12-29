6da5f433acd98164bb5724323e63df67771f887a
Serdar Arazov <serdararazik@gmail.com>
2021-12-28 22:00:08 -0500

    movie1 branch final work is done, ready to merge not knowing spouse moved location of movie

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world, EU4");
         // watching more movie in bedroom
+        // ready to go back to living room
     }
 }
