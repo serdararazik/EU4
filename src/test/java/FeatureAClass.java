66033df04e4296acd0386bf0a3d1f2855d34e48c
Serdar Arazov <serdararazik@gmail.com>
2021-12-28 21:23:59 -0500

    more work on feature A on a new branch

--- src/test/java/FeatureAClass.java
+++ src/test/java/FeatureAClass.java
@@ -2,5 +2,6 @@ public class FeatureAClass {
 
     public static void main(String[] args) {
         System.out.println("Awesome work for A started");
+        System.out.println("EU4 is learning more git with review");
     }
 }
