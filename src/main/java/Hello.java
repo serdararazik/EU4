d53fe7313c3bc94ec150a6ded546e418e8d4a9b2
Serdar Arazov <serdararazik@gmail.com>
2021-12-28 22:08:11 -0500

    movie2 branch 20 min commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world B22");
         //movie on the phone 10 minutes
+        //movie on the phone 20 minutes
     }
 }
